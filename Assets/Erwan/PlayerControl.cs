9a3dc755b7330690f54c042937cfb29f39c70e52
Erwan <erwantassin91@mail.com>
2019-09-20 09:18:00 +0200

    Reinported previous assets

---
+++ Assets/Erwan/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerControl : MonoBehaviour
+{
+    Player player;
+    public Rigidbody2D rb;
+
+    public float speedMod = 1;
+    public bool canMove;
+    public bool canInteract;
+    public Vector2 moveVector;
+
+
+    private void Start()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            moveVector.x = player.GetAxis("Horizontal");
+            moveVector.y = player.GetAxis("Vertical");
+        }
+        else
+        {
+            moveVector.Normalize();
+        }
+
+        if (player.GetButtonDown("Interact") && canInteract)
+        {
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveVector != Vector2.zero)
+        {
+            rb.velocity = new Vector2(moveVector.x * speedMod, moveVector.y * speedMod);
+        }
+        else
+        {
+            rb.velocity -= rb.velocity * 0.25f;
+        }
+    }
+}
