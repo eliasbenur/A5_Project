f9ae638381a1d3a92c2863dbdb627a9f2b6d9713
jelaip <jelaiperdudsl@gmail.com>
2019-09-20 09:54:50 +0200

    porte Shader
    porte Shader

--- Assets/Pierrick/Script/Porte.cs
+++ Assets/Pierrick/Script/Porte.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
+//[RequireComponent(typeof(Animator))]
 public class Porte :Obj
 {
     public bool open = false;
@@ -23,11 +23,13 @@ public class Porte :Obj
     void Open()
     {
         Debug.Log("open");
-        anim.SetBool("open", true);
+        //anim.SetBool("open", true);
+        transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z + 90);
     }
     void Close()
     {
         Debug.Log("close");
-        anim.SetBool("open", false);
+        //anim.SetBool("open", false);
+        transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z - 90);
     }
 }
---
+++ Assets/Pierrick/Script/TextScale.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+[RequireComponent(typeof(Text))]
+public class TextScale : MonoBehaviour
+{
+#if UNITY_EDITOR
+    
+    public class TextScaleEditor : Editor
+    {
+        private Text txt { get { return (target as Text); } }
+        public float tailleFront = 10;
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUI.BeginChangeCheck();
+            int lenghtTxt = txt.text.Length;
+            txt.rectTransform.sizeDelta= new Vector2(lenghtTxt * tailleFront, txt.rectTransform.sizeDelta.y);
+            if (EditorGUI.EndChangeCheck())
+                EditorUtility.SetDirty(txt);
+        }
+    }
+#endif
+}
