d327c83232e49c87b6c518ace39fedc9cab33bec
Erwan <erwantassin91@mail.com>
2019-10-03 11:25:46 +0200

    merge

---
+++ Assets/End.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameManager.done)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
