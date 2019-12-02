83ff07fe45f6cfee15c94befcfa77e873791b116
Elias <eliasbenur@hotmail.com>
2019-12-02 10:36:11 +0100

    IA Optimizer

---
+++ Assets/MAIN/Scripts/Tools/OptimizerManager.cs
@@ -0,0 +1,46 @@
+﻿using BehaviorDesigner.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptimizerManager : MonoBehaviour
+{
+    Transform toFollow;
+    public List<GameObject> guardList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toFollow = ObjectRefs.Instance.player.transform;
+        Bounds CameraBound = Outils.OrthographicBounds(Camera.main);
+        GetComponent<BoxCollider2D>().size = CameraBound.size + new Vector3(20,20,0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = toFollow.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 11) // Guard
+        {
+            guardList.Add(collision.gameObject);
+            collision.gameObject.GetComponent<FOV_vBT>().enabled = true;
+            collision.gameObject.GetComponent<BehaviorTree>().enabled = true;
+            collision.gameObject.GetComponent<GuardIAController_v2>().enabled = true;
+        }   
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 11) // Guard
+        {
+            guardList.Remove(collision.gameObject);
+            collision.gameObject.GetComponent<FOV_vBT>().enabled = false;
+            collision.gameObject.GetComponent<BehaviorTree>().enabled = false;
+            collision.gameObject.GetComponent<GuardIAController_v2>().enabled = false;
+        }
+    }
+}
--- Assets/MAIN/Scripts/Tools/Outils.cs
+++ Assets/MAIN/Scripts/Tools/Outils.cs
@@ -100,4 +100,14 @@ public static class Outils
 
         return pathLength / 2;
     }
+
+    public static Bounds OrthographicBounds(Camera camera)
+    {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = camera.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            camera.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
+    }
 }
