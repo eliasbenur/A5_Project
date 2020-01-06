925a48f6c2658d559cece7e9b387d4c594b6fd19
Elias <eliasbenur@hotmail.com>
2020-01-06 20:27:31 +0100

    Error and Warning (Main Scene)

--- Assets/MAIN/Scripts/GameManager.cs
+++ Assets/MAIN/Scripts/GameManager.cs
@@ -9,9 +9,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public GameObject currentObjective_Canvas;
-    public GameObject objectiveDone_Canvas;
-
     //If the player has taken a treasure in the Lvl
     public bool objectiveDone;
 
@@ -22,8 +19,8 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-
-        ObjectRefs.Instance.objectivesData = SaveGame.Load<ObjectRemaning>("ObjectRemaining");
+        if(SaveGame.Exists("ObjectRemaining"))
+            ObjectRefs.Instance.objectivesData = SaveGame.Load<ObjectRemaning>("ObjectRemaining");
 
         //Desactive treasure already stolen
         foreach (stringAndBool sb in ObjectRefs.Instance.objectivesData.obj)
@@ -34,17 +31,6 @@ public class GameManager : MonoBehaviour
                 GameObject.Find(sb.name).SetActive(false);
             }
         }
-    }
-
-    public void CheckObjective(GameObject newObject)
-    {
-        //If the treasure taken is in the list of objectives
-        /*if (Outils.GameObjectExistInArray(ObjectRefs.Instance.objectivesRef_List, newObject))
-        {
-            currentObjective_Canvas.gameObject.SetActive(false);
-            objectiveDone_Canvas.gameObject.SetActive(true);
-            objectiveDone = true;
-        }*/
     }
 
     public void RestartGame()
